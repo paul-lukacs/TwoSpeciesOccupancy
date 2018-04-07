83f2b9a7ad9fb20cbb55165e93d19f4bd6b4fc2d
PMonterroso <37364099+PMonterroso@users.noreply.github.com>
2018-04-07 19:02:52 +0100

    Update JAGSmodel_interaction.R

--- JAGSmodel_interaction.R
+++ JAGSmodel_interaction.R
@@ -8,32 +8,37 @@
 # zA = occupancy state of subordinate species
 # psiB = Pr(zB =1)
 # psiAB = Pr(zA=1|zB=1)
-# psi Ab = Pr(zA=1|zB=0)
+# psi A = Pr(zA=1|zB=0)
 # pB = Pr(yB=1|zB=1)
 # pAB = Pr(yA=1|zA=1,zB=1)
-# pAb = Pr(yA=1|zA=1,zB=0)
+# pA = Pr(yA=1|zA=1,zB=0)
 
 model{
 
   # priors
   b0.psiB ~ dnorm(0,0.001)T(-10,10)
   b0.psiAB ~ dnorm(0,0.001)T(-10,10)
-  b0.psiAb ~ dnorm(0,0.001)T(-10,10)
+  b0.psiA ~ dnorm(0,0.001)T(-10,10)
   b0.pB ~ dnorm(0,0.001)T(-10,10)
   b0.pAB ~ dnorm(0,0.001)T(-10,10)
-  b0.pAb ~ dnorm(0,0.001)T(-10,10)  
+  b0.pA ~ dnorm(0,0.001)T(-10,10)  
+  sigma.psi ~  dunif(0,4)
+  tau.psi <- 1/(sigma.psi*sigma.psi)
   
+  for(i in 1:narea){
+    error.psi[i] ~ dnorm(0, tau.psi)
+  }
   
   # tranformations
   for(i in 1:nSites){
-    logit(psiB[i]) <- b0.psiB
-    logit(psiAB[i]) <- b0.psiAB
-    logit(psiAb[i]) <- b0.psiAb
+    logit(psiB[i]) <- b0.psiB + error.psi[areaID[i]] # include random effects of study area on dominant species occupancy
+    logit(psiAB[i]) <- b0.psiAB                        
+    logit(psiA[i]) <- b0.psiA + error.psi[areaID[i]] # include random effects of study area on subordinate species occupancy
     
     for( j in 1:nocc ){
       logit(pB[i,j]) <- b0.pB
       logit(pAB[i,j]) <- b0.pAB
-      logit(pAb[i,j]) <- b0.pAb
+      logit(pA[i,j]) <- b0.pAb
     }
   }
   
@@ -43,13 +48,13 @@ model{
   
   for (i in 1:nSites){
     zB[i] ~ dbern( psiB[i] )
-    zA[i] ~ dbern( zB[i] * psiAB[i] + (1-zB[i]) * psiAb[i] )
+    zA[i] ~ dbern( zB[i] * psiAB[i] + (1-zB[i]) * psiA[i] )
     
     # observation model
     for(j in 1:nocc){
       yB[i,j] ~ dbern( zB[i] * pB[i,j] )
       yA[i,j] ~ dbern( zA[i] * ( zB[i] * pAB[i,j] + 
-                              (1-zB[i]) * pAb[i,j] )
+                              (1-zB[i]) * pA[i,j] )
                       )
     }
   }
@@ -57,8 +62,8 @@ model{
   
   #fit <- sum(Presi[,])# Discrepancy for actual data set
   #fit.new <- sum(Presi.new[,]) 		# Discrepancy for replicate data set
-  ZZBa <- sum(zBa[])
+  #ZZBa <- sum(zBa[])
   # derived parameters
-  phi <- psiB[1]*psiAB[1]/(psiB[1]*(psiB[1]*psiAB[1]+(1-psiB[1])*psiAb[1]))
+  phi <- psiB[1]*psiAB[1]/(psiB[1]*(psiB[1]*psiAB[1]+(1-psiB[1])*psiA[1]))
   
 }
