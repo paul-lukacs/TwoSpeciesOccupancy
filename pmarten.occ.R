555f113ee16901bcbb522d77955660eb2402d8bf
PMonterroso <37364099+PMonterroso@users.noreply.github.com>
2018-04-17 15:43:12 +0100

    Add files via upload
    code for single species occupancy models and results

---
+++ pmarten.occ.R
@@ -0,0 +1,189 @@
+#
+# Occupancy analysis
+#
+
+library(mcmcplots)
+library(R2jags)
+
+setwd( "/Users/pedromonterroso/Documents/Trabalho/Publications/Papers/In prep/1.spatiotemporal interactions among Iberian carnivores/3.analyses/1.rcode/occ/single.species/6.pmarten" )
+
+
+# set detection histories and covariate data
+source("/Users/pedromonterroso/Documents/Trabalho/Publications/Papers/In prep/1.spatiotemporal interactions among Iberian carnivores/3.analyses/1.rcode/occ/single.species/data.R")
+
+EH <- y$ymmt[,c(5:68)]
+nocc <- ncol(EH)
+nSites <- nrow(EH)
+naive.mmt <- sum(as.integer(apply(EH,1,sum, na.rm=T)>0))/nrow(EH)
+
+
+# random effect
+areaID <- as.numeric(siteCovs[,3])
+narea <- max(areaID)
+
+# detection covariates
+trail <- detCovs$trail[,5]
+ctr <- detCovs$ctr[,c(5:68)]
+
+# site covariates
+EVI <- siteCovs$EVInb
+rod.d <- siteCovs$rod.ha
+rod.ts <- siteCovs$rod.ts
+d.sett <- siteCovs$d.sett
+lat <- siteCovs$lat
+season <- as.numeric(as.factor(siteCovs$season))-1
+
+
+occ.data <- list( y=EH, 
+                  nSites=nSites,
+                  nocc=nocc,
+                  EVI=EVI,
+                  rod.d=rod.d,
+                  rod.ts=rod.ts,
+                  d.sett=d.sett,
+                  season=season,
+                  lat=lat,
+                  trail=trail,
+                  areaID=areaID,
+                  narea=narea
+)
+
+occ.inits <- function(){
+  list(
+    z = as.integer(apply(EH,1,sum, na.rm=T)>0),
+    b0.psi = runif(1, -3, 3),
+    b1.psi = runif(1, -3, 3),
+    b2.psi = runif(1, -3, 3),
+    b3.psi = runif(1, -3, 3),
+    b4.psi = runif(1, -3, 3),
+    b5.psi = runif(1, -3, 3),
+    b6.psi = runif(1, -3, 3),
+    b0.p = runif(1, -3, 3),
+    b1.p = runif(1, -3, 3),
+    sigma = runif(1, 0, 0.1)
+  )
+}
+
+
+occ.parm <- c( "b0.psi",
+               "b1.psi",
+               "b2.psi",
+               "b3.psi",
+               "b4.psi",
+               "b5.psi",
+               "b6.psi",
+               "b0.p",
+               "b1.p",
+               "mean.psi",
+               "mean.p",
+               "totOcc",
+               "fit",
+               "fit.new",
+               "sigma"
+)
+
+# set up for MCMC run
+ni <- 50000 #500000
+nt <- 10     #3
+nb <- 20000 #20000
+nc <- 3
+
+# run the MCMC chain in JAGS
+pmarten <- jags( occ.data, 
+               occ.inits,
+               occ.parm,
+               "pmarten.model.R",
+               n.chains=nc, 
+               n.iter=ni, 
+               n.burnin=nb,
+               n.thin=nt
+)
+
+mcmcplot(pmarten)
+
+
+# assess model fit
+plot(pmarten$BUGSoutput$sims.list$fit, pmarten$BUGSoutput$sims.list$fit.new, main = "", xlab = 
+       "Discrepancy for actual data set", ylab = "Discrepancy for perfect data sets", las = 1, bty='n', pch=16, cex=0.8, 
+     xlim=c(min(c(pmarten$BUGSoutput$sims.list$fit, pmarten$BUGSoutput$sims.list$fit.new)),max(c(pmarten$BUGSoutput$sims.list$fit, pmarten$BUGSoutput$sims.list$fit.new))),
+     ylim=c(min(c(pmarten$BUGSoutput$sims.list$fit, pmarten$BUGSoutput$sims.list$fit.new)),max(c(pmarten$BUGSoutput$sims.list$fit, pmarten$BUGSoutput$sims.list$fit.new))))
+abline(0,1, lwd = 2, col='red')
+
+mean(pmarten$BUGSoutput$sims.list$fit.new > pmarten$BUGSoutput$sims.list$fit) # acceptable values are those that fall between 0.1 and 0.9. Extremely low (<0.1) and high (>0.9) reveal structural failure of model fit
+
+#function to calculate the inverse logit
+expit <- function(x){
+  exp(x)/(1+exp(x))
+}
+
+#function to obtain the mode
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+## function to obtain the percentile for specific value 
+ecdf_fun <- function(x,perc) ecdf(x)(perc)
+
+# retain variables relevant at the 85% credible interval
+m <- data.frame(pmarten$BUGSoutput$summary)
+for(i in 1:nrow(m)){
+  m$zero.perctl[i] <- ecdf_fun(pmarten$BUGSoutput$sims.matrix[,i],0)
+  if(m$zero.perctl[i]<0.075|m$zero.perctl[i]>0.925){
+    m$retain[i]="yes"}else{
+      m$retain[i]="no"
+    }
+}
+m <- m[c(1,3,2,4:9,13:14),]  
+m$covariate <- c("p.intercept",
+                 "p.trail",
+                 "psi.intercept",
+                 "EVI",
+                 "latitude",
+                 "rodent.d",
+                 "rodent.ts",
+                 "d.sett",
+                 "season",
+                 "mean.p",
+                 "mean.psi")
+
+write.csv(m,"pmarten.csv")
+
+#generate data frame with estimates for each psi beta
+x <- data.frame(pmarten$BUGSoutput$sims.matrix)[,c("b1.psi","b2.psi","b3.psi","b4.psi","b5.psi","b6.psi")]
+x <- round(x,2)
+x1 <- data.frame(c(x$b1.psi,x$b2.psi,x$b3.psi,x$b4.psi,x$b5.psi,x$b6.psi))
+colnames(x1) <- "Estimate"
+x1$names <- c(rep("EVI",nrow(x)),
+              rep("latitude",nrow(x)),
+              rep("rodent dens",nrow(x)),
+              rep("rodent ts",nrow(x)),
+              rep("dst settlements",nrow(x)),
+              rep("season",nrow(x)))
+
+#install.packages("devtools")
+#library(devtools)
+#install_github("easyGgplot2", "kassambara")
+library(easyGgplot2)
+
+p <- ggplot(x1, aes(x=Estimate, fill=names)) +
+  geom_density(alpha=0.4) +
+  scale_fill_brewer(palette = "Paired") +
+  theme_minimal() +
+  scale_x_continuous(limits = c(-2.5, 2.5)) + 
+  geom_vline(xintercept = 0, linetype = "dashed", colour = "black") +
+  ggtitle("Pine marten") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  facet_wrap(~ names,nrow = 3,ncol = 3) +
+  theme(legend.position="none")
+
+print(p)
+pdf("pmarten.pdf")
+print(p)
+dev.off()
+
+# Get codes for colours in a palette
+library("RColorBrewer")
+brewer.pal(12, "Paired")
+# to display that palette:
+display.brewer.pal(12, "Paired")
---
+++ wildcat.model.R
@@ -0,0 +1,57 @@
+#
+# Occupancy JAGS code
+#
+
+model{
+  
+  # priors
+  b0.psi ~ dnorm(0,0.001)T(-10,10)
+  b1.psi ~ dnorm(0,0.001)T(-10,10)
+  b2.psi ~ dnorm(0,0.001)T(-10,10)
+  b3.psi ~ dnorm(0,0.001)T(-10,10)
+  b4.psi ~ dnorm(0,0.001)T(-10,10)
+  b5.psi ~ dnorm(0,0.001)T(-10,10)
+  b6.psi ~ dnorm(0,0.001)T(-10,10)
+  b0.p ~ dnorm(0,0.001)T(-10,10)
+  b1.p ~ dnorm(0,0.001)T(-10,10)
+  sigma ~  dunif(0,4)
+  tau <- 1/(sigma*sigma) 
+  
+  for(i in 1:narea){
+    error[i] ~ dnorm(0, tau)
+  }
+  
+  
+  # tranformations
+  for( i in 1:nSites ){
+    logit(psi[i]) <- b0.psi + b1.psi*EVI[i] + b2.psi*rbbt.d[i] + b3.psi*rbbt.ts[i] + b4.psi*rbbt.d[i]*rbbt.ts[i] + b5.psi*d.sett[i] + b6.psi*season[i] + error[areaID[i]]
+    for( j in 1:nocc ){
+      logit(p[i,j]) <- b0.p + b1.p*trail[i] 
+    }
+  }
+  
+  # likelihood
+  for( i in 1:nSites ){
+    # process model
+    z[i] ~ dbern( psi[i] )
+    
+    # observation model
+    for( j in 1:nocc ){
+      y[i,j] ~ dbern( z[i]*p[i,j] )
+      
+      #Computation of fit statistic (for Bayesian p-value)
+      Presi[i,j] <- abs(y[i,j]-p[i,j])	 # Absolute residual
+      y.new[i,j]~dbern(z[i]*p[i,j])
+      Presi.new[i,j] <- abs(y.new[i,j]-p[i,j])
+    }
+  }
+  
+  fit <- sum(Presi[,])# Discrepancy for actual data set
+  fit.new <- sum(Presi.new[,]) 		# Discrepancy for replicate data set
+  
+  # derived parameters
+  totOcc <- sum(z[])
+  logit(mean.psi) <- b0.psi
+  logit(mean.p) <- b0.p
+  
+}
